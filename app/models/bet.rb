1294169d88b116a7151e5b45acf5a878afefa8f4
Jaime Rodas <jaime@surrealista.mx>
2016-06-05 16:38:24 -0500

    Mejoramos validaciones

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,7 @@
 class Bet < ApplicationRecord
   belongs_to :user
   belongs_to :match
+
+  validates :home, presence: true, numericality: true
+  validates :away, presence: true, numericality: true
 end
