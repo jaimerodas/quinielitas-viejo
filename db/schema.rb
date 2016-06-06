ff593f2a740c529924b24405beb05d556618e1c4
Jaime Rodas <jaime@surrealista.mx>
2016-06-05 21:39:17 -0500

    Agregamos remember_digest a usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605215840) do
+ActiveRecord::Schema.define(version: 20160606004723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160605215840) do
     t.string   "uid"
     t.string   "provider"
     t.string   "auth_token"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
