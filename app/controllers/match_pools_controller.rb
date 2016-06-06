4e17800ace118d2ccc92472ca33ba3ef1b1427ea
Jaime Rodas <jaime@surrealista.mx>
2016-06-06 13:20:06 -0500

    Ya podemos crear quinielas

--- app/controllers/match_pools_controller.rb
+++ app/controllers/match_pools_controller.rb
@@ -3,9 +3,17 @@ class MatchPoolsController < ApplicationController
   end
 
   def new
+    @match_pool = MatchPool.new
   end
 
   def create
+    @match_pool = MatchPool.new(match_pool_params)
+
+    if @match_pool.save
+      redirect_to match_pool_matches_path(@match_pool)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -15,5 +23,11 @@ class MatchPoolsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def match_pool_params
+    params.require(:match_pool).permit(:name)
   end
 end
