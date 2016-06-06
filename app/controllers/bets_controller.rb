17560eec18fa9194e57854403eea5ca0733e0a87
Jaime Rodas <jaime@surrealista.mx>
2016-06-06 10:53:32 -0500

    Creamos bets vacías

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -8,9 +8,22 @@ class BetsController < ApplicationController
   end
 
   def create
+    if current_user.bets.empty?
+      @matches = Match.all
+
+      @matches.each do |m|
+        Bet.create(
+          user: current_user,
+          match: m
+        )
+      end
+    end
+
+    redirect_to edit_bets_path
   end
 
   def edit
+    @bets = current_user.bets
   end
 
   def update
