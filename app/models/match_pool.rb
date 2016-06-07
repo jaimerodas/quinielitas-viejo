2b9af185e94d32de1b84e60bc1b106daed96b035
Jaime Rodas <jaime@surrealista.mx>
2016-06-07 00:02:13 -0500

    Con métodos de conveniencia

--- app/models/match_pool.rb
+++ app/models/match_pool.rb
@@ -1,3 +1,32 @@
 class MatchPool < ApplicationRecord
   has_many :matches
+
+  def accepts_bets?
+    ( self.bets_opened_at and self.bets_closed_at.nil? )
+  end
+
+  def accepts_matches?
+    self.bets_opened_at.nil?
+  end
+
+  def open_betting!
+    unless self.bets_opened_at
+      self.bets_opened_at = Time.now
+      self.save
+    end
+  end
+
+  def close_betting!
+    if self.bets_opened_at
+      self.bets_closed_at = Time.now
+      self.save
+    end
+  end
+
+  def status
+    return 'editando' if self.accepts_matches?
+    return 'apostando' if self.accepts_bets?
+    return 'jugando'
+  end
+
 end
