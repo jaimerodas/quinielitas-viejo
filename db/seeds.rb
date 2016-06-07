b8ae51077538c824dd7d28a20e03af03dd8639db
Jaime Rodas <jaime@surrealista.mx>
2016-06-06 23:37:10 -0500

    Agregamos partidos

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,49 @@ teams = [
 teams.each do |t|
   Team.create(name: t)
 end
+
+quiniela = MatchPool.create(name: 'Eurocopa 2016')
+
+quiniela.matches.create [
+  {home_team_id: 1, away_team_id: 2, when: Time.new(2016, 06, 10, 11, 00, 00, '-05:00')},
+  {home_team_id: 3, away_team_id: 4, when: Time.new(2016, 06, 11, 13, 45, 00, '-05:00')},
+  {home_team_id: 2, away_team_id: 4, when: Time.new(2016, 06, 15, 11, 00, 00, '-05:00')},
+  {home_team_id: 1, away_team_id: 3, when: Time.new(2016, 06, 15, 13, 45, 00, '-05:00')},
+  {home_team_id: 2, away_team_id: 3, when: Time.new(2016, 06, 19, 13, 45, 00, '-05:00')},
+  {home_team_id: 4, away_team_id: 1, when: Time.new(2016, 06, 19, 13, 45, 00, '-05:00')},
+
+  {home_team_id: 5, away_team_id: 6, when: Time.new(2016, 06, 11, 11, 00, 00, '-05:00')},
+  {home_team_id: 7, away_team_id: 8, when: Time.new(2016, 06, 11, 13, 45, 00, '-05:00')},
+  {home_team_id: 8, away_team_id: 6, when: Time.new(2016, 06, 15, 11, 00, 00, '-05:00')},
+  {home_team_id: 7, away_team_id: 5, when: Time.new(2016, 06, 16, 13, 45, 00, '-05:00')},
+  {home_team_id: 8, away_team_id: 5, when: Time.new(2016, 06, 20, 13, 45, 00, '-05:00')},
+  {home_team_id: 6, away_team_id: 7, when: Time.new(2016, 06, 20, 13, 45, 00, '-05:00')},
+
+  {home_team_id: 9, away_team_id: 10, when: Time.new(2016, 06, 12, 11, 00, 00, '-05:00')},
+  {home_team_id: 11, away_team_id: 12, when: Time.new(2016, 06, 12, 13, 45, 00, '-05:00')},
+  {home_team_id: 12, away_team_id: 10, when: Time.new(2016, 06, 16, 11, 00, 00, '-05:00')},
+  {home_team_id: 11, away_team_id: 9, when: Time.new(2016, 06, 16, 13, 45, 00, '-05:00')},
+  {home_team_id: 12, away_team_id: 9, when: Time.new(2016, 06, 21, 13, 45, 00, '-05:00')},
+  {home_team_id: 10, away_team_id: 11, when: Time.new(2016, 06, 21, 13, 45, 00, '-05:00')},
+
+  {home_team_id: 13, away_team_id: 14, when: Time.new(2016, 06, 12, 11, 00, 00, '-05:00')},
+  {home_team_id: 15, away_team_id: 16, when: Time.new(2016, 06, 13, 13, 45, 00, '-05:00')},
+  {home_team_id: 16, away_team_id: 13, when: Time.new(2016, 06, 17, 11, 00, 00, '-05:00')},
+  {home_team_id: 15, away_team_id: 14, when: Time.new(2016, 06, 17, 13, 45, 00, '-05:00')},
+  {home_team_id: 16, away_team_id: 14, when: Time.new(2016, 06, 21, 13, 45, 00, '-05:00')},
+  {home_team_id: 13, away_team_id: 15, when: Time.new(2016, 06, 21, 13, 45, 00, '-05:00')},
+
+  {home_team_id: 17, away_team_id: 18, when: Time.new(2016, 06, 13, 11, 00, 00, '-05:00')},
+  {home_team_id: 19, away_team_id: 20, when: Time.new(2016, 06, 13, 13, 45, 00, '-05:00')},
+  {home_team_id: 20, away_team_id: 18, when: Time.new(2016, 06, 17, 11, 00, 00, '-05:00')},
+  {home_team_id: 19, away_team_id: 17, when: Time.new(2016, 06, 18, 13, 45, 00, '-05:00')},
+  {home_team_id: 20, away_team_id: 17, when: Time.new(2016, 06, 22, 13, 45, 00, '-05:00')},
+  {home_team_id: 18, away_team_id: 19, when: Time.new(2016, 06, 22, 13, 45, 00, '-05:00')},
+
+  {home_team_id: 21, away_team_id: 22, when: Time.new(2016, 06, 14, 11, 00, 00, '-05:00')},
+  {home_team_id: 23, away_team_id: 24, when: Time.new(2016, 06, 14, 13, 45, 00, '-05:00')},
+  {home_team_id: 24, away_team_id: 22, when: Time.new(2016, 06, 18, 11, 00, 00, '-05:00')},
+  {home_team_id: 23, away_team_id: 21, when: Time.new(2016, 06, 18, 13, 45, 00, '-05:00')},
+  {home_team_id: 24, away_team_id: 21, when: Time.new(2016, 06, 22, 13, 45, 00, '-05:00')},
+  {home_team_id: 22, away_team_id: 23, when: Time.new(2016, 06, 22, 13, 45, 00, '-05:00')}
+]
