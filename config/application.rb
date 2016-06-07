08dbfccbe6491fce193ced829695c88aeec5cd49
Jaime Rodas <jaime@surrealista.mx>
2016-06-07 10:34:07 -0500

    Configuramos email

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Quinielitas
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'America/Mexico_City'
+    config.mail = config_for(:mail)
   end
 end
