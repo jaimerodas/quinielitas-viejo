7960042a1548a2fbe05c918743f65d0713f29269
Jaime Rodas <jaime@surrealista.mx>
2016-06-07 23:55:07 -0500

    Agregamos helper para errores

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def field_error errors, name
+    if errors[name.to_sym].first
+      return "<span class=\"help error\">#{errors[name.to_sym].first.capitalize}</span>".html_safe
+    end
+  end
+
 end
