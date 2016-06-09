2683aaf250368a813f47798fb709c536eb59754e
Jaime Rodas <jaime@surrealista.mx>
2016-06-08 22:02:14 -0500

    Agregamos mailer a production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "quinielitas_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'quinielitas.co', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Rails.configuration.mail
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
