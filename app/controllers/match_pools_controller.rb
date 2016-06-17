129a1a5d02043c2dc21e08e7f3af49e96541b581
Jaime Rodas <jaime@surrealista.mx>
2016-06-16 19:31:06 -0500

    No quiero recalcular mi score a cada vuelta

--- app/controllers/match_pools_controller.rb
+++ app/controllers/match_pools_controller.rb
@@ -9,7 +9,6 @@ class MatchPoolsController < ApplicationController
   def show
     get_scoreboard
     @matches = matches_with_bets_for(current_user)
-    @match_pool.participants.first.recalculate_points
   end
 
   def new
