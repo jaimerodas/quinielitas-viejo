047ddeb0c8cfb545d03be42365997d8f8e4a5a46
Jaime Rodas <jaime@surrealista.mx>
2016-06-21 18:38:50 -0500

    Actualizamos graph api version

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.configuration.facebook['app_id'], Rails.configuration.facebook['secret'], scope: 'email'
+  provider :facebook,
+    Rails.configuration.facebook['app_id'],
+    Rails.configuration.facebook['secret'],
+    scope: 'email',
+    client_options: {
+      site: "https://graph.facebook.com/v2.2",
+      authorize_url: "https://www.facebook.com/v2.2/dialog/oauth"
+    }
 end
 
 OmniAuth.config.logger = Rails.logger
